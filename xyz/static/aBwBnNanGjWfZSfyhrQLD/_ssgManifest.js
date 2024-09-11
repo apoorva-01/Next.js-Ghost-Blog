ae2a80e46483230b3df871a46be99b4fc876b0a4
Apoorva Verma <apoorva.verma@oracle.com>
2024-09-11 13:52:45 +0530

    xyz deleted

--- xyz/static/aBwBnNanGjWfZSfyhrQLD/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fguides\u002F[slug]","\u002Fposts","\u002Fposts\u002F[slug]","\u002Fposts\u002Ftagged\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
