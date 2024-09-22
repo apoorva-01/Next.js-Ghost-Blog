3b69a8f33d775cb0e4247f77f762f71985e0831e
Apoorva Verma <apoorva.verma@oracle.com>
2024-09-22 16:38:45 +0530

    make responsive

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,48 +11,44 @@ const About: NextPage = () => {
         description="MySQL and Heatwave Release Engineer @Oracle">
         <Prose>
 
+          <div className="about-container">
+            <div className="about-intro">
+              <span>
+                <p>
+                  Hey there! I’m excited to share my DevOps journey with you 🚀 <br /><br />
+                  I’ve been diving into the world of DevOps, learning the ins and outs of making development and operations work better together. <br />
 
-          <p style={{ display: 'flex', alignItems: 'center' }}>
-            <span>
-              <p>
-                Hey there! I’m excited to share my DevOps journey with you 🚀 <br /><br />
-                I’ve been diving into the world of DevOps, learning the ins and outs of making development and operations work better together. <br />
+                  <b>Along the way, I’ve picked up some cool tips, tricks, and a few lessons learned the hard way—So why not share them? 😉</b>
+                </p>
+              </span>
+              <Image
+                src={siteConfig?.authorImage}
+                alt={siteConfig?.authorName}
+                width={300}
+                height={300}
+                  className="about-image"
+              />
+            </div>
+            <p>
+              As a professional immersed in DevOps, I’ve had the opportunity to work with cutting-edge technologies like Oracle Cloud Infrastructure (OCI), Jenkins, Docker, and more. <br /><br />
+              My blog is a reflection of the knowledge I’ve gained through hands-on projects, certifications, and real-world challenges.
+            </p>
 
-                <b>Along the way, I’ve picked up some cool tips, tricks, and a few lessons learned the hard way—So why not share them? 😉</b>
-              </p>
-            </span>
-            <Image
-              src={siteConfig?.authorImage}
-              alt={siteConfig?.authorName}
-              width={300}
-              height={300}
-              style={{
-                borderRadius: '50%', // Circular shape
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', // Box shadow
-                marginLeft: '15px' // Spacing between text and image
-              }}
-            />
-          </p>
-          <p>
-            As a professional immersed in DevOps, I’ve had the opportunity to work with cutting-edge technologies like Oracle Cloud Infrastructure (OCI), Jenkins, Docker, and more. <br /><br />
-            My blog is a reflection of the knowledge I’ve gained through hands-on projects, certifications, and real-world challenges.
-          </p>
+            <p>
+              I’ve been playing around with tools like Jenkins, Docker, and Oracle Cloud Infrastructure (OCI), and I’m constantly exploring new ways to make things run smoother and faster. <br /><br />
+              This blog is my space to jot down everything I’m learning, from the basics to the more advanced stuff, in the hopes that it might help someone else out there.
 
-          <p>
-            I’ve been playing around with tools like Jenkins, Docker, and Oracle Cloud Infrastructure (OCI), and I’m constantly exploring new ways to make things run smoother and faster. <br /><br />
-            This blog is my space to jot down everything I’m learning, from the basics to the more advanced stuff, in the hopes that it might help someone else out there.
+            </p>
+            <p>
 
-          </p>
-          <p>
-
-            Whether you’re just getting started with DevOps or you’ve been at it for a while, I’m glad you’re here. <br />
-            Let’s learn, experiment, and maybe even mess up a little—because that’s how we grow, right?
-          </p>
+              Whether you’re just getting started with DevOps or you’ve been at it for a while, I’m glad you’re here. <br />
+              Let’s learn, experiment, and maybe even mess up a little—because that’s how we grow, right?
+            </p>
 
 
 
 
-
+          </div>
 
         </Prose>
       </Page>
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,60 +25,60 @@ const Posts: NextPage<PostProps> = ({ guides }) => {
 
           return (
             <div className="mt-5 block bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" key={idx}>
-              <div className="grid grid-cols-3 gap-3">
-                {/* Image */}
-                {guideIntroPost.feature_image?
-                <>
-                <a href={`/guides/${guideData.guide_name}`} className="flex-shrink-0">
-                  <Image
-                    className="rounded-lg"
-                    src={guideIntroPost.feature_image}
-                    alt="Description of the image"
-                    width={500}
-                    height={300}
-                  />
-                </a>
-                </>:null}
-
-                {/* Intro */}
-                <div className="p-3">
-                  <h2 className=" text-lg font-semibold text-gray-900 dark:text-white">
-                    What you&apos;ll learn
-                  </h2>
-                  <p className="font-normal text-gray-700 dark:text-gray-400">
-                    {guideData.guide_description}
-                  </p>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+              {/* Image */}
+              {guideIntroPost.feature_image ? (
+                <div className="md:col-span-1">
+                  <a href={`/guides/${guideData.guide_name}`} className="flex-shrink-0">
+                    <Image
+                     className="rounded-lg w-full" // Added w-full for 100% width
+                      src={guideIntroPost.feature_image}
+                      alt="Description of the image"
+                      width={500}
+                      height={300}
+                    />
+                  </a>
                 </div>
-                {/* List */}
-                <div className="p-3">
-                  <h2 className=" text-lg font-semibold text-gray-900 dark:text-white">
-                    {" "}
-                    {posts.length} Resources
-                  </h2>
-
-                  <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
-                    {posts.slice(0, 6).map((post: any, index: any) => (
-                      <li className="flex items-center" key={index}>
-                        <svg
-                          className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="currentColor"
-                          viewBox="0 0 20 20"
-                        >
-                          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
-                        </svg>
-                        <h2 className="text-sm">
-                          <Link href={`/posts/${post.slug}`}>
-                            {post.title}
-                          </Link>
-                        </h2>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+              ) : null}
+          
+              {/* Intro */}
+              <div className="p-3 md:col-span-1">
+                <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  What you&apos;ll learn
+                </h2>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  {guideData.guide_description}
+                </p>
+              </div>
+          
+              {/* List */}
+              <div className="p-3 md:col-span-1">
+                <h2 className="text-lg font-semibold text-gray-900 dark:text-white">
+                  {posts.length} Resources
+                </h2>
+                <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                  {posts.slice(0, 6).map((post: any, index: any) => (
+                    <li className="flex items-center" key={index}>
+                      <svg
+                        className="w-3.5 h-3.5 me-2 text-green-500 dark:text-green-400 flex-shrink-0"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                      </svg>
+                      <h2 className="text-sm">
+                        <Link href={`/posts/${post.slug}`}>
+                          {post.title}
+                        </Link>
+                      </h2>
+                    </li>
+                  ))}
+                </ul>
               </div>
             </div>
+          </div>
           );
         })}
       </Page>
