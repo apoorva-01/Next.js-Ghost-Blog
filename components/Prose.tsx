e173e0c533a5138001a012b43abb6c44042df96f
Apoorva Verma <apoorva.verma@oracle.com>
2024-09-24 19:05:57 +0530

    code scc added

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,9 +1,9 @@
-export const Prose: React.FC<{
-  children: React.ReactNode;
-}> = ({ children }) => {
+
+export const Prose: React.FC<{children: React.ReactNode;}> = ({ children }) => {
+  
   return (
     // <div className="max-w-none prose prose-a:text-pink-600 dark:prose-invert">
-         <div className="max-w-none prose dark:prose-invert">
+    <div className="max-w-none prose dark:prose-invert">
       {children}
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
-
+// import 'prismjs/themes/prism.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider
