b739cd8876920f5206633f5c9ecb64aad0467d94
Apoorva Verma <apoorva.verma@oracle.com>
2024-09-24 14:18:40 +0530

    a css color change

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -2,7 +2,8 @@ export const Prose: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
-    <div className="max-w-none prose prose-a:text-pink-600 dark:prose-invert">
+    // <div className="max-w-none prose prose-a:text-pink-600 dark:prose-invert">
+         <div className="max-w-none prose dark:prose-invert">
       {children}
     </div>
   );
