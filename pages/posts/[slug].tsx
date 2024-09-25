ba10c05fae49d61bbe6491cbad7cc5e9e584b5b9
Apoorva Verma <apoorva.verma@oracle.com>
2024-09-25 11:38:48 +0530

    styling added

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -4,11 +4,13 @@ import Link from "next/link";
 import { Page } from "@/components/Page";
 import { Prose } from "@/components/Prose";
 import { cx } from "@/lib/utils";
-import Breadcrumb from '../../components/Breadcrumb';
+import Breadcrumb from "../../components/Breadcrumb";
 import { getPosts, getPostBySlug } from "@/lib/ghost";
 import siteConfig from "@/data/siteConfig";
 import Image from "next/image";
 import { formatDate } from "@/lib/formatDate";
+import styled from "styled-components";
+// import styles from '../../styles/Post.module.css';
 interface ContextProps extends ParsedUrlQuery {
   slug: string;
 }
@@ -20,6 +22,104 @@ interface PostProps {
 }
 
 const Post: NextPage<PostProps> = ({ post, previous, next }) => {
+  console.log(post);
+  const Container = styled.div`
+    // Below is the styling that I want to apply only for posts not global styling
+
+    body {
+      line-height: 1.6;
+      color: #374151;
+      padding: 20px;
+    }
+
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      line-height: 1.4;
+      color: rgb(0, 0, 0);
+      margin-bottom: 10px;
+    }
+
+    h1 {
+      font-size: 2rem;
+      font-weight: 800;
+    }
+
+    h2 {
+      font-size: 1.9rem;
+      font-weight: 700;
+    }
+
+    h3 {
+      font-size: 1.5rem;
+      font-weight: 600;
+    }
+
+    h4 {
+      font-size: 1.125rem;
+      font-weight: 600;
+    }
+
+    h5 {
+      font-size: 1.125rem;
+    }
+
+    h6 {
+      font-size: 1rem;
+    }
+
+    p {
+      font-size: 1rem;
+      margin-bottom: 20px;
+      font-weight: 400;
+    }
+
+    ul {
+      list-style-type: disc;
+      margin-left: 20px;
+      margin-bottom: 15px;
+    }
+
+    li {
+      margin-bottom: 10px;
+    }
+
+    figure {
+      margin: 20px 0;
+      text-align: center;
+    }
+
+    img {
+      max-width: 100%;
+      height: auto;
+      border: 3px solid #ddd;
+    }
+
+    hr {
+      border: 0;
+      border-top: 2px solid #ccc;
+      margin: 20px 0;
+    }
+
+    /* strong {
+       color: #0399e3;
+    } */
+
+    blockquote {
+      margin: 20px 0;
+      padding: 10px 20px;
+      border-left: 5px solid #ddd; /* Left border for emphasis */
+      background-color: #f9f9f9; /* Light background color */
+      color: #333; /* Text color */
+      font-style: italic; /* Italic style for the quote */
+    }
+
+
+  
+  `;
+
   return (
     <>
       <Breadcrumb />
@@ -41,7 +141,11 @@ const Post: NextPage<PostProps> = ({ post, previous, next }) => {
             {siteConfig?.authorName} <br />
             {post.updated_at ? (
               <time
-                className={cx("block mb-2", "text-gray-500", "dark:text-gray-400")}
+                className={cx(
+                  "block mb-2",
+                  "text-gray-500",
+                  "dark:text-gray-400"
+                )}
               >
                 {formatDate(post.updated_at)}
               </time>
@@ -50,12 +154,17 @@ const Post: NextPage<PostProps> = ({ post, previous, next }) => {
         )}
       </div>
 
-
-      <Page title={post.title} description={post.excerpt} date={post.updated_at} slug={post.slug} >
-
-        <Prose>
+      <Page
+        title={post.title}
+        description={post.excerpt}
+        date={post.updated_at}
+        slug={post.slug}
+      >
+        {/* <Prose> */}
+        <Container>
           <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </Prose>
+        </Container>
+        {/* </Prose> */}
         {previous || next ? (
           <nav
             className={cx(
